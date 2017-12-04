56990b4d528fdeb39e9bcdb9a099582e758db6b9
Hannah Zentner <hazentner@gmail.com>
2017-12-03 23:01:37 -0500

    server.js changes

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 var express = require('express')
 var path = require('path')
 
-
 var app = express()
-
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
+var index = require('./index');
 
 app.get('/jobs', function(req, res) {
   res.sendFile(path.join(__dirname + '/jobs.html'));
 });
+
+app.use('/', index);
 
 app.listen(3000);
 
