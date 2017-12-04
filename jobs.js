fb7a879652c8365c054c5e51f92a1dd4d628b6ac
Hannah Zentner <hazentner@gmail.com>
2017-12-04 14:34:50 -0500

    add jobs.js backend for displaying and adding jobs

---
+++ jobs.js
@@ -0,0 +1,30 @@
+var express = require('express')
+var path = require('path')
+var router = express.Router(); 
+var mongojs = require('mongojs')
+var db = mongojs('mongodb://hannah:hannah@ds044587.mlab.com:44587/jobsdb', ['jobs'])
+
+router.get('/jobs', function(req, res) {
+  db.collection('jobs').find().toArray(function(err, items){
+    if(err) {
+       res.send(err);
+    }
+    else {
+      res.render('jobs.ejs', {jobs: items});
+     }
+  });
+});
+
+router.post('/newjob', function(req, res) {
+ var job = req.body;
+ db.collection('jobs').save(job, function(err, job) {
+   if (err)
+      res.send(err);
+    else {
+       res.redirect('/joblist/jobs');
+   }
+});
+
+});
+ 
+ module.exports = router;
