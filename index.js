3fd877b67cd71d100c9fa677648d50260717b469
Hannah Zentner <hazentner@gmail.com>
2017-12-05 20:53:14 -0500

    replace bcrypt with password-hash

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express')
 var path = require('path')
 var router = express.Router();
-var bcrypt = require('bcrypt')
+//var bcrypt = require('bcrypt')
+var pwhash = require('password-hash')
 var mongojs = require('mongojs')
 var db = mongojs('mongodb://hannah:hannah@ds044587.mlab.com:44587/jobsdb', ['users'])
 
@@ -18,13 +19,14 @@ router.post('/login', function(req, res) {
     res.sendFile(path.join(__dirname + '/invalid.html'));  
 }
   else {
-     bcrypt.compare(req.body.password, user.password, function(err, pw) {
+     var pw = pwhash.verify(req.body.password, user.password);
+     //bcrypt.compare(req.body.password, user.password, function(err, pw) {
      if (pw) 
        res.redirect('/joblist/' + req.body.username + '/jobs');
      else
        res.sendFile(path.join(__dirname + '/invalid.html'));
      
-   });
+   //});
   }
   });
 });
@@ -35,19 +37,20 @@ router.post('/register', function(req, res) {
 
 router.post('/signup', function(req, res) {
    var user = req.body;
-   bcrypt.hash(req.body.password, 10, function(err, pw) {
-   if (err)
-     console.log(err);
-   else {
-   db.collection('users').save({email: req.body.email, username: req.body.username, password: pw}, function(err, user) {
+   //bcrypt.hash(req.body.password, 10, function(err, pw) {
+   var hashed = pwhash.generate(req.body.password);
+   //if (err)
+     //console.log(err);
+   //else {
+   db.collection('users').save({email: req.body.email, username: req.body.username, password: hashed}, function(err, user) {
    if (err)
       res.send(err);
    else
       res.redirect('/joblist/' + req.body.username + '/jobs');
 });
-}
+//}
 
-});
+//});
 });
 
 module.exports = router;
